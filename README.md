e117cbcbf6fa05e9d3e37f1984c2374a2edeaf1c
tartarchan <94968465+tartarchan@users.noreply.github.com>
2022-02-02 23:15:34 +0800

    reorganised enemy_data folder

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
+
+Changes in enemy IDs
+
+- Heart of Surging Flame
+  
+  - P-01 - P-10
+
+- Main Story 9
+  
+  - CPS-MDR
+    
+    
--- pages/stages/[name].js
+++ pages/stages/[name].js
@@ -1,32 +1,42 @@
 import Layout from "../../components/layout";
 import { getAllStageIds, getStageData } from "../../lib/stages";
 import Head from "next/head";
-import Image from 'next/image'
+import Image from "next/image";
 
 export async function getStaticProps({ params }) {
-	const stageData = await getStageData(params.name);
-	return {
-		props: {
-			stageData,
-		},
-	};
+  const stageData = await getStageData(params.name);
+  return {
+    props: {
+      stageData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-	const paths = getAllStageIds();
-	return {
-		paths,
-		fallback: false,
-	};
+  const paths = getAllStageIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default function Stage({ stageData }) {
+	console.log(stageData);
+
+
+
+
 	return (
-		<Layout>
-			<Head>
-				<title>{stageData.name}</title>
-			</Head>
-			{stageData.name}
-		</Layout>
-	);
+    <Layout>
+      <Head>
+        <title>{stageData.name}</title>
+      </Head>
+      <Image
+        src={`/images/maps/${stageData.name}.jpg`}
+        alt={stageData.name}
+		height="600px"
+		width="1062px"
+      ></Image>
+    </Layout>
+  );
 }
