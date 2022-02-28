1b4848d7874c03ea809b41b102931317bc488ec9
Chor <97508567+tanch-p@users.noreply.github.com>
2022-02-28 14:01:01 +0800

    added twitter to about

--- pages/about.js
+++ pages/about.js
@@ -19,21 +19,41 @@ export default function About({ allStagesData }) {
           <p className="lg:max-w-xl my-3">
             このサイトは補正が掛かった敵のデータを可視化するように、統合戦略のために作りました。統合戦略はまだ先なので、試しに危機契約のステージを加えました。まだまだ開発中なので、もしバグや不具合または改善したところを見つかったら、メールをお願いいたします。
           </p>
-          <div className="border-y border-gray-200 border-collapse py-2">
-            <span className="font-semibold">管理人：</span>{" "}
-            <a
-              href="https://github.com/tanch-p"
-              className="underline text-blue-700"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Chor
-            </a>
-          </div>
-          <div className="border-y border-gray-200 border-collapse py-2">
-            <span className="font-semibold">サイトメール：</span>
-            <span>prtschan@gmail.com</span>
-          </div>
+          <dl className="flex flex-wrap flex-row w-full overflow-hidden m-0 p-0  max-w-[700px]">
+            <dt className="border-y border-gray-200 border-collapse py-2 font-semibold w-[25%] md:w-[17%]">
+              管理人
+            </dt>
+            <dd className="border-y border-gray-200 border-collapse py-2 w-[75%] md:w-[83%]">
+              <a
+                href="https://github.com/tanch-p"
+                className="underline text-blue-700"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Chor
+              </a>
+            </dd>
+            <dt className="border-y border-gray-200 border-collapse py-2 font-semibold w-[25%] md:w-[17%]">
+              メール
+            </dt>
+            <dd className="border-y border-gray-200 border-collapse py-2 w-[75%] md:w-[83%]">
+              prtschan@gmail.com
+            </dd>
+
+            <dt className="border-y border-gray-200 border-collapse py-2 font-semibold w-[25%] md:w-[17%]">
+              ツイーター
+            </dt>
+            <dd className="border-y border-gray-200 border-collapse py-2 w-[75%] md:w-[83%]">
+              <a
+                href="https://twitter.com/prts_chan"
+                className="underline text-blue-700"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @prts_chan
+              </a>
+            </dd>
+          </dl>
         </div>
       ) : (
         <div id="home-container" className="min-h-[100vh] font-sans">
@@ -41,26 +61,46 @@ export default function About({ allStagesData }) {
           <p className="lg:max-w-xl my-3">
             This website was created with the goal of being able to see the
             actual numbers of enemy stats with modifiers in mind, for Integrated
-            Strategies (IS) gameplay. As IS is only coming few months later, this
-            beta version for CC was made. If you find any bugs/mistakes or have
-            any feedback, please kindly drop an email to me or open a Github
-            issue.
+            Strategies (IS) gameplay. As IS is only coming few months later,
+            this beta version for CC was made. If you find any bugs/mistakes or
+            have any feedback, please kindly drop an email to me or open a
+            Github issue.
           </p>
-          <div className="border-y border-gray-200 border-collapse py-2">
-            <span className="font-semibold">Webmaster：</span>{" "}
-            <a
-              href="https://github.com/tanch-p"
-              className="underline text-blue-700"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Chor
-            </a>
-          </div>
-          <div className="border-y border-gray-200 border-collapse py-2">
-            <span className="font-semibold">Site Email：</span>
-            <span>prtschan@gmail.com</span>
-          </div>
+          <dl className="flex flex-wrap flex-row w-full overflow-hidden m-0 p-0  max-w-[700px]">
+            <dt className="border-y border-gray-200 border-collapse py-2 font-semibold w-[25%] md:w-[17%]">
+              Webmaster
+            </dt>
+            <dd className="border-y border-gray-200 border-collapse py-2 w-[75%] md:w-[83%]">
+              <a
+                href="https://github.com/tanch-p"
+                className="underline text-blue-700"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Chor
+              </a>
+            </dd>
+            <dt className="border-y border-gray-200 border-collapse py-2 font-semibold w-[25%] md:w-[17%]">
+              Email
+            </dt>
+            <dd className="border-y border-gray-200 border-collapse py-2 w-[75%] md:w-[83%]">
+              prtschan@gmail.com
+            </dd>
+
+            <dt className="border-y border-gray-200 border-collapse py-2 font-semibold w-[25%] md:w-[17%]">
+              Twitter
+            </dt>
+            <dd className="border-y border-gray-200 border-collapse py-2 w-[75%] md:w-[83%]">
+              <a
+                href="https://twitter.com/prts_chan"
+                className="underline text-blue-700"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @prts_chan
+              </a>
+            </dd>
+          </dl>
         </div>
       )}
     </Layout>
